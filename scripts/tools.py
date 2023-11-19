2a983cbf4ff9580cf64d9fad953ee1dc0f790a8b
kevinchen85 <b04901035@ntu.edu.tw>
2023-11-18 22:01:38 -0500

    modify tools.py to check ft232h address

--- scripts/tools.py
+++ scripts/tools.py
@@ -8,16 +8,16 @@ def find_gpio_addr():
 
     # GPIO Board USB addresses
     FT232H_list = UsbTools.find_all([(0x403, 0x6014)])
-    gpio0_addr = "ftdi://ftdi:232h:" + hex(FT232H_list[0][0].bus)[2:].zfill(2) + ":"+hex(FT232H_list[0][0].address)[2:].zfill(2)+"/1"
-    gpio1_addr = "ftdi://ftdi:232h:" + hex(FT232H_list[1][0].bus)[2:].zfill(2) + ":"+hex(FT232H_list[1][0].address)[2:].zfill(2)+"/1"
+    addr0 = "ftdi://ftdi:232h:" + hex(FT232H_list[0][0].bus)[2:].zfill(2) + ":"+hex(FT232H_list[0][0].address)[2:].zfill(2)+"/1"
+    addr1 = "ftdi://ftdi:232h:" + hex(FT232H_list[1][0].bus)[2:].zfill(2) + ":"+hex(FT232H_list[1][0].address)[2:].zfill(2)+"/1"
 
     # Print out bridge address
-    print("USB Address of the first board to be checked is: " + gpio0_addr)
-    print("USB Address of the second board to be checked is: " + gpio1_addr)
+    print("USB Address of 1st board to be checked is: " + addr0)
+    print("USB Address of 2ns board to be checked is: " + addr1)
     
-    return gpio0_addr, gpio1_addr
+    return addr0, addr1
 
-def simple_write(addr):
+def simple_write(addr, data):
     """
     Simple write to check maps to which board
 
@@ -29,11 +29,12 @@ def simple_write(addr):
     gpio = GpioMpsseController()
     gpio.configure(addr, direction=0xffff, frequency=1e6)
     
-    # Write D0 to 0
-    gpio.write(0x0000)
-    # Write D0 to 1
-    gpio.write(0x0001)
+    # Write data
+    gpio.write(data)
     gpio.close()
 
-input_addr, output_addr = find_gpio_addr()
-simple_write(input_addr)
+addr0, addr1 = find_gpio_addr()
+# Write D0 to 0
+simple_write(addr0, 0x0000)
+# Write D0 to 1
+simple_write(addr1, 0x0001)

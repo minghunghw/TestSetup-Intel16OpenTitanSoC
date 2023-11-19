d3f1b5655a5dc5d8930b2b192317ba34c918ad61
kevinchen85 <b04901035@ntu.edu.tw>
2023-11-19 16:23:00 -0500

    complete gpio write example and try spi write with logic analyzer

--- scripts/tools.py
+++ scripts/tools.py
@@ -1,29 +1,60 @@
 from pyftdi.gpio import GpioMpsseController
 from pyftdi.usbtools import UsbTools
 
-def find_gpio_addr():
+"""
+FT232H Device Pinout
+
+========== ======= ====== ======== ========== ======
+   BUS     BitBang  UART    I2C       SPI      JTAG 
+========== ======= ====== ======== ========== ======
+  ADBUS0    GPIO0    TxD    SCK      SCLK       TCK   
+  ADBUS1    GPIO1    RxD    SDA/O    MOSI       TDI   
+  ADBUS2    GPIO2    RTS    SDA/I    MISO       TDO   
+  ADBUS3    GPIO3    CTS    GPIO3    CS0        TMS   
+  ADBUS4    GPIO4    DTR    GPIO4    CS1/GPIO4       
+  ADBUS5    GPIO5    DSR    GPIO5    CS2/GPIO5        
+  ADBUS6    GPIO6    DCD    GPIO6    CS3/GPIO6        
+  ADBUS7    GPIO7    RI     RSCK     CS4/GPIO7  RCLK 
+  ACBUS0    GPIO8           GPIO8    GPIO8
+  ACBUS1    GPIO9           GPIO9    GPIO9
+  ACBUS2    GPIO10          GPIO10   GPIO10
+  ACBUS3    GPIO11          GPIO11   GPIO11
+  ACBUS4    GPIO12          GPIO12   GPIO12
+  ACBUS5    GPIO13          GPIO13   GPIO13
+  ACBUS6    GPIO14          GPIO14   GPIO14
+  ACBUS7    GPIO15          GPIO15   GPIO15
+
+"""
+
+"""
+FT232H Device USB Address
+"""
+in_addr = "ftdi://ftdi:232h:00:ff/1"
+out_addr = "ftdi://ftdi:232h:00:fe/1"
+
+def find_usb_addr():
     """"
-    Checking the addresses of each bridge board
+    Check the addresses of each bridge board
     """
 
     # GPIO Board USB addresses
     FT232H_list = UsbTools.find_all([(0x403, 0x6014)])
-    addr0 = "ftdi://ftdi:232h:" + hex(FT232H_list[0][0].bus)[2:].zfill(2) + ":"+hex(FT232H_list[0][0].address)[2:].zfill(2)+"/1"
-    addr1 = "ftdi://ftdi:232h:" + hex(FT232H_list[1][0].bus)[2:].zfill(2) + ":"+hex(FT232H_list[1][0].address)[2:].zfill(2)+"/1"
+    addr = []
+    for i in range(len(FT232H_list)):
+        addr.append("ftdi://ftdi:232h:" + hex(FT232H_list[i][0].bus)[2:].zfill(2) + ":"+hex(FT232H_list[i][0].address)[2:].zfill(2)+"/1")
+        
+        # Print out bridge address
+        print("USB Address of board " + str(i) + " is: " + addr[i])
 
-    # Print out bridge address
-    print("USB Address of 1st board to be checked is: " + addr0)
-    print("USB Address of 2ns board to be checked is: " + addr1)
-    
-    return addr0, addr1
+    return addr
 
-def simple_write(addr, data):
+def simple_gpio_write(addr, data):
     """
-    Simple write to check maps to which board
+    Simple GPIO write to a board according to address
 
     Parameters:
-        direction  (16 bits): 0 for input, 1 for output
-        bitmap     (16 bits): C7:C0, D7:D0
+        direction     (16 bits): 0 for input, 1 for output
+        write bitmap  (16 bits): [C7-C0, D7-D0]
     """
     # Initialize GPIO
     gpio = GpioMpsseController()
@@ -33,8 +64,36 @@ def simple_write(addr, data):
     gpio.write(data)
     gpio.close()
 
-addr0, addr1 = find_gpio_addr()
-# Write D0 to 0
-simple_write(addr0, 0x0000)
-# Write D0 to 1
-simple_write(addr1, 0x0001)
+def check_board_address():
+    """
+    Check which address maps to which board
+
+    In this example, we have two FT232H boards connected to PC
+    """
+    addr = find_usb_addr()
+    # Write board 1 C7-C0, D7-D0 to 0
+    simple_gpio_write(addr[0], 0x0000)
+    # Write board 2 C7-C0, D7-D0 to 0
+    simple_gpio_write(addr[1], 0x0000)
+
+def simple_spi_write(addr, data):
+    """
+    Simple SPI write to a board according to address
+    """
+    # Initalize SPI
+    spi = SpiController()
+    spi.configure(addr, frequency=1e6, mode=0)
+    slave = spi.get_port(cs=0)
+    slave.write(data)
+
+def check_spi_2_logic_analyzer():
+    """
+    Check SPI transaction send correctly
+
+    In this example, we have saleae logic analyzer connected to the FT232H board
+    """
+    addr = find_usb_addr()
+    # Write board 1 one byte transaction 0x64 (100)
+    simple_spi_write(addr[0], 0x64)
+    # Write board 1 one byte transaction 0x64 (100)
+    simple_spi_write(addr[1], 0x64)

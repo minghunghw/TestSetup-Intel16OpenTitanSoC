4242eefdf99caad3976fb4c34ffbc30c95ff9ec1
minghung <minghung@umich.edu>
2023-11-21 14:01:38 -0500

    fix spi_master latch error for spi_sdo signal

--- src/stimuli/spi_master.v
+++ src/stimuli/spi_master.v
@@ -31,11 +31,12 @@ module spi_master(
     reg [4:0] cycle_w, cycle_r;
     reg done_w, done_r;
     reg spi_sclk_w, spi_sclk_r;
-    reg spi_sdo;
+    reg spi_sdo_w, spi_sdo_r;
     reg spi_cs_w, spi_cs_r;
 
     assign done = done_r;
     assign spi_sclk = spi_sclk_r;
+    assign spi_sdo = spi_sdo_r;
     assign spi_cs = spi_cs_r;
 
     // Stimuli ROM
@@ -59,6 +60,7 @@ module spi_master(
         cycle_w = cycle_r;
         done_w = done_r;
         spi_sclk_w = spi_sclk_r;
+        spi_sdo_w = spi_sdo_r;
         spi_cs_w = spi_cs_r;
 
         case (state_r)
@@ -71,64 +73,66 @@ module spi_master(
             end
             CMD: begin
                 spi_sclk_w = ~spi_sclk_r;
-                if (cycle_r == 0 && spi_sclk_r) begin
-                    state_w = ADDR;
-                    cycle_w = 31;
-                end
                 if (spi_sclk_r) begin
                     cycle_w = cycle_r - 1;
                     if (cycle_r >= 4)
-                        spi_sdo = 0;
-                    else
-                        spi_sdo = cmd[count_r][cycle_r];
+                        spi_sdo_w = 0;
+                    else if (cycle_r == 0) begin
+                        spi_sdo_w = 0; // addr[count_r][31]
+                        state_w = ADDR;
+                        cycle_w = 31;
+                    end else
+                        spi_sdo_w = cmd[count_r][cycle_r - 1];
                 end
             end
             ADDR: begin
                 spi_sclk_w = ~spi_sclk_r;
-                if (cycle_r == 0 && spi_sclk_r) begin
-                    if (cmd[count_r] == 11) begin
-                        state_w = DUMMY;
-                        cycle_w = 33;
-                    end else begin
-                        state_w = DATA;
-                        cycle_w = 31;
-                    end
-                end
                 if (spi_sclk_r) begin
                     cycle_w = cycle_r - 1;
                     if (cycle_r >= 8)
-                        spi_sdo = 0;
-                    else
-                        spi_sdo = addr[count_r][cycle_r];
+                        spi_sdo_w = 0;
+                    else if (cycle_r == 0) begin
+                        if (cmd[count_r] == 11) begin
+                            spi_sdo_w = 0;
+                            state_w = DUMMY;
+                            cycle_w = 33;
+                        end else begin
+                            spi_sdo_w = data[count_r][31];
+                            state_w = DATA;
+                            cycle_w = 31;
+                        end
+                    end else
+                        spi_sdo_w = addr[count_r][cycle_r - 1];
                 end
             end
             DUMMY: begin
                 spi_sclk_w = ~spi_sclk_r;
-                if (cycle_r == 0 && spi_sclk_r) begin
-                    state_w = DATA;
-                    cycle_w = 31;
-                end
                 if (spi_sclk_r) begin
                     cycle_w = cycle_r - 1;
-                    spi_sdo = 0;
+                    spi_sdo_w = 0;
+                    if (cycle_r == 0) begin
+                        state_w = DATA; // Read Data
+                        cycle_w = 31;
+                    end
                 end
             end
             DATA: begin
                 spi_sclk_w = ~spi_sclk_r;
-                if (cycle_r == 0 && spi_sclk_r) begin
-                    if (count_r == TRANSACTION_SIZE-1) begin
-                        state_w = DONE;
-                        done_w = 1;
-                        spi_cs_w = 1;
-                    end else begin
-                        state_w = CMD;
-                        cycle_w = 7;
-                        count_w = count_r + 1;
-                    end
-                end
                 if (spi_sclk_r) begin
                     cycle_w = cycle_r - 1;
-                    spi_sdo = data[count_r][cycle_r];
+                    spi_sdo_w = data[count_r][cycle_r];
+                    if (cycle_r == 0) begin
+                        spi_sdo_w = 0; // cmd[count_r][7]
+                        if (count_r == TRANSACTION_SIZE-1) begin
+                            state_w = DONE;
+                            done_w = 1;
+                            spi_cs_w = 1;
+                        end else begin
+                            state_w = CMD;
+                            cycle_w = 7;
+                            count_w = count_r + 1;
+                        end
+                    end
                 end
             end
             DONE: begin
@@ -144,7 +148,7 @@ module spi_master(
             cycle_r     <= 0;
             done_r      <= 0;
             spi_sclk_r  <= 0;
-            spi_sdo     <= 0;
+            spi_sdo_r   <= 0;
             spi_cs_r    <= 1;
         end else begin
             count_r     <= count_w;
@@ -152,6 +156,7 @@ module spi_master(
             cycle_r     <= cycle_w;
             done_r      <= done_w;
             spi_sclk_r  <= spi_sclk_w;
+            spi_sdo_r   <= spi_sdo_w;
             spi_cs_r    <= spi_cs_w;
         end
     end

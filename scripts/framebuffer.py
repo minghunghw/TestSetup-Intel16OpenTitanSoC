514211b3b91d95597cf9e0a45b7b28a18991b1fd
minghung <minghung@umich.edu>
2023-12-09 21:09:53 -0500

    fix the text for sh1122, add figure support

--- scripts/framebuffer.py
+++ scripts/framebuffer.py
@@ -8,7 +8,7 @@ def text_to_framebuffer(text, width, height, font_size=12):
     draw = ImageDraw.Draw(image)
 
     # Choose a font with a specific size
-    font = ImageFont.load_default().font_variant(size=font_size)
+    font = ImageFont.truetype("arial.ttf", font_size)
 
     # Draw text on the image
     text_position = (0, 0)
@@ -16,5 +16,27 @@ def text_to_framebuffer(text, width, height, font_size=12):
 
     # Convert the image to a byte array
     byte_array = image.tobytes()
+    return byte_array
 
+def figure_to_framebuffer(figure_path, width, height):
+    # Create a new image with a black background (1-bit mode)
+    image = Image.new("1", (width, height), 0)  # "1" indicates 1-bit mode, 0 for black background
+
+    # Get a drawing context
+    draw = ImageDraw.Draw(image)
+
+    # Load the figure (replace "figure.png" with the path to your figure)
+    figure = Image.open(figure_path)
+
+    # Resize the figure to fit within the specified width and height
+    figure.thumbnail((width, height))
+
+    # Calculate the position to center the figure on the background
+    position = ((width - figure.width) // 2, (height - figure.height) // 2)
+
+    # Paste the figure onto the image
+    image.paste(figure, position)
+
+    # Convert the image to a byte array (if needed)
+    byte_array = image.tobytes()
     return byte_array
--- scripts/opentitan_testLCD.py
+++ scripts/opentitan_testLCD.py
@@ -1,13 +1,18 @@
 import framebuffer as fb
 from sh1122_ctrl import sh1122 as sh_ctrl
 
-def test_SH1122():
+def test_SH1122_text():
     sh = sh_ctrl()
-    frame = fb.text_to_framebuffer("Hello World!", 1024, 64, 48)
+    frame = fb.text_to_framebuffer("U M i c h  O p e n F A S o C", 1024, 64, 48)
     sh.write_data(frame)
 
-def test_lcd_logo():
+def test_SH1122_logo():
+    sh = sh_ctrl()
+    frame = fb.figure_to_framebuffer("fig/umich.png", 1024, 64)
+    sh.write_data(frame)
+
+def test_chip_logo():
     sh = sh_ctrl()
     
-# test_SH1122()
-# test_lcd_logo()
+# test_SH1122_text()
+# test_chip_logo()

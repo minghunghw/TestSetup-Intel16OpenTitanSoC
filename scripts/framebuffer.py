0f55049e1fc02586e79f0791e3f78157e81a91f3
minghung <minghung@umich.edu>
2023-12-09 20:48:18 -0500

    tune framebuffer to fit 1024x64 oled lcd display

--- scripts/framebuffer.py
+++ scripts/framebuffer.py
@@ -1,30 +1,20 @@
 from PIL import Image, ImageDraw, ImageFont
 
-def text_to_framebuffer(width, height, text):
-    # Create a new image with a white background (1-bit mode)
-    image = Image.new("1", (width, height), 1)  # "1" indicates 1-bit mode
+def text_to_framebuffer(text, width, height, font_size=12):
+    # Create a new image with a black background (1-bit mode)
+    image = Image.new("1", (width, height), 0)  # "1" indicates 1-bit mode
 
     # Get a drawing context
     draw = ImageDraw.Draw(image)
 
-    # Choose a font
-    font = ImageFont.load_default()
+    # Choose a font with a specific size
+    font = ImageFont.load_default().font_variant(size=font_size)
 
     # Draw text on the image
-    text_position = (10, 10)
-    draw.text(text_position, text, font=font, fill=0)  # Using 0 for black text color
+    text_position = (0, 0)
+    draw.text(text_position, text, font=font, fill=1)  # Using 1 for white text color
 
     # Convert the image to a byte array
     byte_array = image.tobytes()
 
     return byte_array
-
-# Example usage
-framebuffer_width = 320
-framebuffer_height = 240
-text_content = "Hello, Pillow!"
-
-byte_array = text_to_framebuffer(framebuffer_width, framebuffer_height, text_content)
-
-# Print the first few bytes as an example
-print(byte_array[:20])
--- scripts/opentitan_testLCD.py
+++ scripts/opentitan_testLCD.py
@@ -1,12 +1,13 @@
+import framebuffer as fb
 from sh1122_ctrl import sh1122 as sh_ctrl
 
 def test_SH1122():
     sh = sh_ctrl()
-    sh.write_data(0x12)
+    frame = fb.text_to_framebuffer("Hello World!", 1024, 64, 48)
+    sh.write_data(frame)
 
 def test_lcd_logo():
     sh = sh_ctrl()
-
     
 # test_SH1122()
 # test_lcd_logo()
